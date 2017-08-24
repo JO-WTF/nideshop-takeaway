1f46a219c1c44f4314b65b70122945903949c500
Jo.WTF <zhongzhyu@gmail.com>
2017-08-24 17:35:53 +0100

    用户体验优化2(bugfix)
    添加相关商品时使用新函数

--- pages/goods/goods.js
+++ pages/goods/goods.js
@@ -342,5 +342,48 @@ Page({
     this.setData({
       number: this.data.number + 1
     });
-  }
+  },
+
+  addRelatedToCart: function (event) {
+    var that = this;
+    util.request(api.GoodsDetail, { id: event.target.dataset.id }).then(function (res) {
+      if (res.errno === 0) {
+        that.setData({
+          goods: res.data.info,
+          attribute: res.data.attribute,
+          specificationList: res.data.specificationList,
+          productList: res.data.productList,
+        });
+        if (res.data.productList.length > 1) {
+          wx.showToast({
+            title: '点击商品图片选择商品规格',
+          })
+        }
+        else {
+          util.request(api.CartAdd, { goodsId: event.target.dataset.id, number: 1, productId: res.data.productList[0].id }, "POST")
+            .then(function (res) {
+              let _res = res;
+              if (_res.errno == 0) {
+                wx.showToast({
+                  title: '添加成功',
+                  duration: 500
+                });
+                that.setData({
+                  cartGoodsCount: _res.data.cartTotal.goodsCount
+                });
+              } else {
+                wx.showToast({
+                  image: '/static/images/icon_error.png',
+                  title: _res.errmsg,
+                  mask: true
+                });
+              }
+
+            });
+        }
+      }
+    });
+    console.log(event.target.dataset.id)
+    //添加到购物车
+  },
 })
